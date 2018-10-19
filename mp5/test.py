080ac6785256f3059e0b2cc2fe2c6e972c77e681
YicongY <able1994@hotmail.com>
2018-10-19 17:11:11 -0500

    Add

--- mp5/test.py
+++ mp5/test.py
@@ -18,6 +18,41 @@ from PIL import Image
 import sys, getopt
 from torchvision import models as t_models
 
+class TripleDataset(Dataset):
+    def __init__(self,triplelist, root_dir,train, transform=None):
+        self.triplelist = pickle.load(open(triplelist,'rb'))
+        self.root_dir = root_dir
+        self.transform = transform
+        self.train = train
+
+    def __len__(self):
+        return len(self.triplelist)
+
+    def __getitem__(self, idx):
+        if self.train :
+            label = self.triplelist[idx][0].split('_')[0]
+            subdir = label
+            query_image_path = self.root_dir + '/'+ subdir + '/images/' + self.triplelist[idx][0]
+            positive_image_path = self.root_dir + '/'+ subdir + '/images/' + self.triplelist[idx][1]
+            negative_label = self.triplelist[idx][2].split('_')[0]
+            subdir = negative_label
+            negative_image_path = self.root_dir + '/'+ subdir + '/images/' + self.triplelist[idx][2]
+            positive_image = Image.open(positive_image_path).convert('RGB')
+            query_image = Image.open(query_image_path).convert('RGB')
+            negative_image = Image.open(negative_image_path).convert('RGB')
+            sample = {'positive_image': positive_image, 'query_image': query_image, 'negative_image' : negative_image}
+            if self.transform:
+                for i,v in sample.items():
+                    sample[i] = self.transform(v)
+            return sample, self.triplelist[idx]
+        else:
+            query_image_path = self.root_dir + self.triplelist[idx]
+            label_list = pickle.load(open("testlist_label.pkl", 'rb'))
+            query_image = Image.open(query_image_path).convert('RGB')
+            if self.transform:
+                sample = self.transform(query_image)
+            return sample, label_list[idx]
+
 def test(embedding_array,train_image_name):
     embedding_size = 4096
     transform = transforms.Compose(
