6dc4e0e365b7986f58d80648031ff2198e9d16f8
YicongY <able1994@hotmail.com>
2018-10-19 19:27:23 -0500

    Add

--- mp5/sol.py
+++ mp5/sol.py
@@ -286,7 +286,7 @@ def main(pretrain,argv):
             # get the inputs
             #print(len(image_dict))
             data_i , label = data
-            print(len(data_i),"data len")
+            print(data_i.shape)
             positive_image = data_i['positive_image']
             query_image = data_i['query_image']
             negative_image =data_i['negative_image']
