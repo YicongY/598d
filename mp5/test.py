785a42830d9de2ec898f1d6475c398c608e0b6e5
YicongY <able1994@hotmail.com>
2018-10-22 16:31:42 -0500

    add

--- mp5/test.py
+++ mp5/test.py
@@ -51,7 +51,7 @@ class TripleDataset(Dataset):
             query_image = Image.open(query_image_path).convert('RGB')
             if self.transform:
                 sample = self.transform(query_image)
-            return sample, label_list[idx]
+            return sample, label_list[idx], self.triplelist[idx]
 
 def test(embedding_array,train_image_name):
     embedding_size = 4096
@@ -127,4 +127,93 @@ def test(embedding_array,train_image_name):
     print("average acc of testing: ", (count)* 100 /(len(predict_out[1])*30))#test_output.shape[0])
     print('One time: ', time.time()- time3)
 
-test('embedding.pkl', 'train_image_name.pkl')
+def validate(embedding_array,train_image_name):
+    name_path = 'train_image_name_real.pkl'
+    embedding_size = 4096
+    transform = transforms.Compose(
+        [transforms.Resize((224,224)),
+         transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    net = t_models.resnet101(pretrained=True)
+    num_inp = net.fc.in_features
+    net.fc = nn.Linear(num_inp, embedding_size)
+    model_file = Path("model.pt")
+    if model_file.is_file():
+        net.load_state_dict(torch.load("model.pt"))
+        print("load previous model parameters")
+    if torch.cuda.is_available():
+        print('cuda')
+        device = torch.device('cuda:0')
+    else:
+        print('cpu')
+        device = torch.device('cpu')
+
+    net.to(device)
+
+    embedding_array = np.load(embedding_array)
+    print(embedding_array.shape, "embedding array shape")
+    train_image_name = np.load(open(train_image_name, 'rb'))
+    train_image_name_real  = np.load(open(name_path, 'rb'))
+
+    print(len(train_image_name), "image_label_length")
+    time3 = time.time()
+    net.eval()
+    testset = TripleDataset(triplelist = 'testlist.pkl', root_dir = 'tiny-imagenet-200/val/images/', train = 0,
+                             transform = transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size= 5,shuffle=True, num_workers = 4)
+    #label_list = pickle.load(open("testlist_label.pkl", 'rb'))
+    #tree_array = np.vstack((outputs, embedding_array))
+    neigh = KNeighborsClassifier(n_neighbors=10, n_jobs= -1 )
+
+    test_output =[]
+    test_label = []
+    test_name_list = []
+    for i, data in enumerate(testloader, 0):
+        # get the inputs
+        inputs, labels , test_name= data
+
+        inputs = inputs.to(device)
+        outputs = net(inputs)
+        outputs_c = outputs.cpu().data.numpy()
+        del outputs
+        #print(outputs_c.shape)
+        for s_label in range(outputs_c.shape[0]):
+            test_output.append(outputs_c[s_label])
+            test_label.append(labels[s_label])
+            test_name_list.append(test_name[s_label])
+        break
+    time_fit = time.time()
+    print("begin to fit the model")
+    neigh.fit(embedding_array, train_image_name)
+    print("finish_fitting",time.time() - time_fit)
+    time_fit = time.time()
+    print("begin to predict")
+    test_output = np.asarray(test_output)
+    predict_out = neigh.kneighbors(test_output)
+    top_ten = np.array(predict_out[1]).flatten()
+    top_ten_dis = np.array(predict_out[0]).flatten()
+    predict_out_10 = neigh.kneighbors(test_output, n_neighbors=100000)
+    bottom_ten = np.array(predict_out_10[1][-10:]).flatten()
+    bottom_ten_dis = np.array(predict_out_10[0][-10:]).flatten()
+
+    print("finish predict", time.time() - time_fit)
+    with open('testname_list.pkl', 'wb') as f:
+        pickle.dump(test_name_list, f)
+        print("test_name")
+    train_image_name_real = np.array(train_image_name_real)
+    top_ten_name =  list(train_image_name_real[top_ten])
+    bottom_ten_name = list(train_image_name_real[bottom_ten])
+    print("Testname", test_name_list)
+    print("topten", top_ten_name)
+    print("bottomten", bottom_ten_name)
+    with open('topname_list.pkl', 'wb') as f:
+        pickle.dump(top_ten_name, f)
+    with open('botname_list.pkl', 'wb') as f:
+        pickle.dump(bottom_ten_name, f)
+    with open('topname_list_dis.pkl', 'wb') as f:
+        pickle.dump(top_ten_dis, f)
+    with open('botname_list_dis.pkl_dis', 'wb') as f:
+        pickle.dump(bottom_ten_dis, f)
+
+
+validate('embedding.pkl', 'train_image_name.pkl')
